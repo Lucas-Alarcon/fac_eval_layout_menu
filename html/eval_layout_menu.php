d244854667778931ac8c2daa837757895a1e02e4
Lucas-Alarcon <lucas.alarcon@etu.univ-amu.fr>
2018-11-26 11:49:46 +0100

    sprites svg

---
+++ html/eval_layout_menu.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Eval Layout menu</title>
+	<link rel="stylesheet" href="../css/mon_site.css">
+	<style type="text/css" media="screen"></style>
+
+
+</head>
+<body style="background-image: url(../img/accueil.jpg)">
+<div class="cacher_sprite">
+    <?php include ("../img/sprite_symbol_pictos.svg")?>
+</div>
+<nav>
+	<img class="hamburger" alt="menu hamburger" src="../img/hamburger.png">
+	<ul>
+		<li><a href="eval_layout_menu.php"><svg class="imgmenu" aria-hidden="true"><use xlink:href="#accueil"></use></svg>Accueil</a></li>
+		<li><a href="cerise.php"><svg class="imgmenu" aria-hidden="true"><use xlink:href="#cerise"></use></svg>Cerise</a></li>
+		<li><a href="poire.php"><svg class="imgmenu" aria-hidden="true"><use xlink:href="#poire"></use></svg>Poire</a></li>
+	</ul>
+</nav>
+<div class="text"><p>Accueil</p></div>
+</body>
+</html>
---
+++ html/poire.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Eval Layout menu</title>
+    <link rel="stylesheet" href="../css/mon_site.css">
+    <style type="text/css" media="screen">
+    </style>
+</head>
+<body style="background-image: url(../img/Poire.jpg)">
+<nav>
+    <img class="hamburger" alt="menu hamburger" src="../img/hamburger.png">
+    <ul>
+        <li><a href="eval_layout_menu.php"><svg class="imgmenu" aria-hidden="true"><use xlink:href="../img/sprite_symbol_pictos.svg#accueil"></use></svg>Accueil</a></li>
+        <li><a href="cerise.php"><svg class="imgmenu" aria-hidden="true"><use xlink:href="../img/sprite_symbol_pictos.svg#cerise"></use></svg>Cerise</a></li>
+        <li><a href="poire.php"><svg class="imgmenu" aria-hidden="true"><use xlink:href="../img/sprite_symbol_pictos.svg#poire"></use></svg>Poire</a></li>
+    </ul>
+</nav>
+<div class="text"><p>Poire</p></div>
+</body>
+</html>
